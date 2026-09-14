efcb3614981e2f51f49831c209864a131997c14b
TristinSmith06 <tristin.smith@students.fhu.edu>
2026-09-14 09:57:52 -0500

    Add validation

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const contact_submit_toast = document.getElementById("toast");
 const submit_button = document.getElementById("submit_contact");
+const contact_form = document.getElementById("contact-form");
 
 submit_button.addEventListener('click', () => {
-    show_toast();
+    if(contact_form.checkValidity()){
+        show_toast();
+    }
 })
 
 
@@ -20,3 +23,14 @@ function restart_timer(duration = 4000){
 
 contact_submit_toast.addEventListener('pointerenter', () => {clearTimeout(hideTimer)})
 contact_submit_toast.addEventListener('pointerleave', () => {restart_timer()})
+
+let contact_required_text = document.getElementById("contact-consent-text")
+let contact_checkbox = document.getElementById("contact_consent")
+
+contact_checkbox.addEventListener('change', (event) => {
+    if (!event.target.checked) {
+        contact_required_text.style.display = "block";
+    }else{
+        contact_required_text.style.display = "none";
+    }
+});
